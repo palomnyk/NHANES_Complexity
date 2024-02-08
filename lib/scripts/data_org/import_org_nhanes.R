24669b396ce58026339d0dfce87b9014dd140c37
Palomnyk <aaronyerke@gmail.com>
2024-02-07 21:14:38 -0500

    Add comments, physical activity questionaire, demographics, and remove volunteers taking prescriptions.

--- lib/scripts/data_org/import_org_nhanes.R
+++ lib/scripts/data_org/import_org_nhanes.R
@@ -26,8 +26,10 @@ nhanes_names <- function(dl_df, dt_group, nh_tble) {
 }
 
 download_org_nhanes <- function(dt_group, nh_tble) {
-  
+  #downloads and relabels table
   dl_tble <- nhanesA::nhanes(nh_tble)
+  
+  dl_tble <- dl_tble[,sapply(dl_tble, function(x) !all(is.na(x)))]
   
   dl_tble_vars <- nhanesA::nhanesTableVars(data_group=dt_group, 
                                              nh_table = nh_tble, 
@@ -61,6 +63,31 @@ convert_dummy <- function(df){
 #### Establish directory layout and other constants ####
 output_dir <- file.path("output", "tables")
 
+drop_criteria <- c("Dietary recall status" = c("Reported consuming breast-milk"),#DEMO_I
+                   ""
+)
+
+# tables to add:
+#   Physical Activity PAQ_I
+#   Oral Health	OHQ_I
+#   Occupation	OCQ_I
+#   Medical Conditions	MCQ_I
+#   Prescription Medications	RXQ_RX_I
+#   Preventive Aspirin Use	RXQASA_I
+
+#   Volatile Toxicant	VTQ_I VTQ_I
+
+
+
+# Add later:
+#   Reproductive Health	RHQ_I Doc	RHQ_I Data [XPT - 1.2 MB]	February 2018
+#   Sexual Behavior	SXQ_I Doc	SXQ_I Data [XPT - 1.9 MB]	December 2017
+#   Sleep Disorders	SLQ_I Doc	SLQ_I Data [XPT - 360.2 KB]	March 2018
+#   Smoking - Cigarette Use	SMQ_I Doc	SMQ_I Data [XPT - 2.6 MB]	September 2017
+#   Smoking - Household Smokers	SMQFAM_I Doc	SMQFAM_I Data [XPT - 312.9 KB]	September 2017
+#   Smoking - Recent Tobacco Use	SMQRTU_I Doc	SMQRTU_I Data [XPT - 1.5 MB]	September 2017
+#   Smoking - Secondhand Smoke Exposure	SMQSHS_I Doc	SMQSHS_I Data [XPT - 1.1 MB]	September 2017
+
 #### Loading in data ####
 food_codes <- readxl::read_excel(file.path("Data", "WWEIA1516_foodcat_FNDDS.xlsx"), 
                                  trim_ws = T, na = c("", "NA"))
@@ -72,25 +99,29 @@ demo_2015 <- download_org_nhanes("DEMO", "DEMO_I")
 
 demo_2015 <- convert_dummy(demo_2015)
 
-write.csv(demo_2015, file = file.path(output_dir, "demo_2015.csv"),
-          row.names = FALSE)
+# write.csv(demo_2015, file = file.path(output_dir, "demo_2015.csv"),
+#           row.names = FALSE)
+
+PAQ_2015 <- download_org_nhanes("Q", "PAQ_I")
+
+PAQ_2015 <- convert_dummy(PAQ_2015)
+
+RXQ_2015 <- download_org_nhanes("Q", "RXQ_RX_I")
+
+my_RXQ <- RXQ_2015[,1:2]
+
+my_RXQ <- my_RXQ[!duplicated(my_RXQ), ]
+
+no_RX <- my_RXQ[my_RXQ$`In the past 30 days, have you used or taken medication for which a prescription is needed?  Do not include prescription vitamins` == "No", "Respondent sequence number."]
 
 nhanesA::nhanesTables(data_group="DIET", year=2015)
 
-d1_24supp_2015 <- nhanesA::nhanes("DS1IDS_I")
+d1_24supp_2015 <- download_org_nhanes("DIET", "DS1IDS_I")
 
-
-d1_diet_2015 <- nhanesA::nhanes("DR1IFF_I")
-
-d1_diet_2015_vars <- nhanesA::nhanesTableVars(data_group = "DIET",
-                                       nh_table = "DR1IFF_I",
-                                       namesonly = TRUE)
-d1_diet_2015 <- nhanesA::nhanesTranslate(nh_table = "DR1IFF_I",
-                                      d1_diet_2015_vars, data = d1_diet_2015)
-d1_diet_2015 <- nhanes_names(d1_diet_2015, "DIET", "DR1IFF_I")
+d1_diet_2015 <- download_org_nhanes("DIET", "DR1IFF_I")
 
 d1_diet_2015 <- d1_diet_2015[, !sapply(d1_diet_2015, is.factor)]
-d1_diet_2015$`USDA food code` <- food_codes$food_code_description[match(d1_diet_2015$`USDA food code`, food_codes$food_code)]
+d1_diet_2015$`USDA food code` <- food_codes$category_description[match(d1_diet_2015$`USDA food code`, food_codes$food_code)]
 
 dir.create(file.path(output_dir), showWarnings = FALSE)
 write.csv(d1_diet_2015, file = file.path(output_dir, "d1_diet_2015.csv"),
@@ -143,6 +174,48 @@ write.csv(demo_2015, file = file.path(output_dir, "demo_2015_match_bp_SYST1_RF.c
 write.csv(BP_2015, file = file.path(output_dir, "BP_2015_match_diet_SYST1_RF.csv"),
           row.names = FALSE)
 
+demo_d1_diet_2015 <- merge(d1_diet_2015, demo_2015, by = "Respondent sequence number." )
+
+write.csv(demo_d1_diet_2015, file = file.path(output_dir, "demo_d1_diet_2015_match_bp_SYST1_RF.csv"),
+          row.names = FALSE)
+#create no RX datasets
+my_intersect <- intersect(no_RX, my_intersect)
+
+d1_diet_2015 <- d1_diet_2015[d1_diet_2015$`Respondent sequence number.`  %in% my_intersect,]
+BP_2015 <- BP_2015[BP_2015$`Respondent sequence number.` %in% my_intersect,]
+demo_2015 <- demo_2015[demo_2015$`Respondent sequence number.`  %in% my_intersect,]
+
+write.csv(d1_diet_2015, file = file.path(output_dir, "noRx_d1_diet_2015_match_bp_SYST1_RF.csv"),
+          row.names = FALSE)
+write.csv(demo_2015, file = file.path(output_dir, "noRx_demo_2015_match_bp_SYST1_RF.csv"),
+          row.names = FALSE)
+write.csv(BP_2015, file = file.path(output_dir, "noRx_BP_2015_match_diet_SYST1_RF.csv"),
+          row.names = FALSE)
+demo_d1_diet_2015 <- merge(d1_diet_2015, demo_2015, by = "Respondent sequence number." )
+demo_d1_diet_2015 <- demo_d1_diet_2015[, !sapply(demo_d1_diet_2015, is.factor)]
+write.csv(demo_d1_diet_2015, file = file.path(output_dir, "noRx_demo_d1_diet_2015_match_bp_SYST1_RF.csv"),
+          row.names = FALSE)
+
+my_intersect <- intersect(PAQ_2015$`Respondent sequence number.`, my_intersect)
+
+d1_diet_2015 <- d1_diet_2015[d1_diet_2015$`Respondent sequence number.`  %in% my_intersect,]
+BP_2015 <- BP_2015[BP_2015$`Respondent sequence number.` %in% my_intersect,]
+demo_2015 <- demo_2015[demo_2015$`Respondent sequence number.`  %in% my_intersect,]
+
+write.csv(d1_diet_2015, file = file.path(output_dir, "PAQ_noRx_d1_diet_2015_match_bp_SYST1_RF.csv"),
+          row.names = FALSE)
+write.csv(demo_2015, file = file.path(output_dir, "PAQ_noRx_demo_2015_match_bp_SYST1_RF.csv"),
+          row.names = FALSE)
+write.csv(BP_2015, file = file.path(output_dir, "PAQ_noRx_BP_2015_match_diet_SYST1_RF.csv"),
+          row.names = FALSE)
+PAQ_demo_d1_diet_2015 <- merge(PAQ_2015, demo_d1_diet_2015, by = "Respondent sequence number." )
+PAQ_demo_d1_diet_2015 <- PAQ_demo_d1_diet_2015[, !sapply(PAQ_demo_d1_diet_2015, is.factor)]
+write.csv(PAQ_demo_d1_diet_2015, file = file.path(output_dir, "PAQ_noRx_demo_d1_diet_2015_match_bp_SYST1_RF.csv"),
+          row.names = FALSE)
+
+
+
+
 #Short version for testing ML code for completion
 short_rsn <- my_intersect[1:200]
 sh_d1_diet_2015 <- d1_diet_2015[d1_diet_2015$`Respondent sequence number.`  %in% short_rsn,]
@@ -156,13 +229,6 @@ write.csv(sh_demo_2015, file = file.path(output_dir, "short_demo_2015_match_bp_S
 write.csv(sh_BP_2015, file = file.path(output_dir, "short_BP_2015_match_diet_SYST1_RF.csv"),
           row.names = FALSE)
 
-
-#### Create a new diet/demo vs BP dataset by combining diet and demo data ####
-
-demo_d1_diet_2015 <- merge(d1_diet_2015, demo_2015, by = "Respondent sequence number." )
-
-write.csv(demo_d1_diet_2015, file = file.path(output_dir, "demo_d1_diet_2015_match_bp_SYST1_RF.csv"),
-          row.names = FALSE)
 
 #### Create "foods only" dataset ####
 d1_diet_2015 <- nhanesA::nhanes("DR1IFF_I")
