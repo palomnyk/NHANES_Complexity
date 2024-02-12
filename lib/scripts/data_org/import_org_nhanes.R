10e410ec9402a2a976e8831ad6ce6e9c00ff4ef2
Palomnyk <aaronyerke@gmail.com>
2024-02-12 12:13:20 -0500

    save predictor features to file, add PHTHTE dataset.

--- lib/scripts/data_org/import_org_nhanes.R
+++ lib/scripts/data_org/import_org_nhanes.R
@@ -58,7 +58,11 @@ convert_dummy <- function(df){
   })
   return(data.frame(my_list, check.names = FALSE))
 }
-  
+
+save_features <- function(vect = raw_features_used, vec_names = "name", feature_vect){
+  names(feature_vect) <- rep(vec_names, length(feature_vect))
+  return(c(raw_features_used, feature_vect))
+}
   
 #### Establish directory layout and other constants ####
 output_dir <- file.path("output", "tables")
@@ -77,6 +81,15 @@ drop_criteria <- c("Dietary recall status" = c("Reported consuming breast-milk")
 
 #   Volatile Toxicant	VTQ_I VTQ_I
 
+# LAB:
+#   HDL_I
+#   Acrylamide & Glycidamide	AMDGYD_I
+#   Arsenic - Total - Urine	UTAS_I
+#   Cholesterol - Low - Density Lipoprotein (LDL) & Triglycerides	TRIGLY_I
+  # Glyphosate (GLYP) - Urine (Surplus)	SSGLYP_I
+#   Organophosphate Insecticides - Dialkyl Phosphate Metabolites - Urine	OPD_I   
+  
+
 
 
 # Add later:
@@ -92,18 +105,19 @@ drop_criteria <- c("Dietary recall status" = c("Reported consuming breast-milk")
 food_codes <- readxl::read_excel(file.path("Data", "WWEIA1516_foodcat_FNDDS.xlsx"), 
                                  trim_ws = T, na = c("", "NA"))
 
+raw_features_used <- c()
+
 # Download raw NHANES data files ## 
 # https://cran.r-project.org/web/packages/nhanesA/vignettes/Introducing_nhanesA.html
 nhanesA::nhanesTables(data_group="DEMO", year=2015)
 demo_2015 <- download_org_nhanes("DEMO", "DEMO_I")
-
+raw_features_used <- save_features(raw_features_used, "DEMO_I", colnames(demo_2015))
 demo_2015 <- convert_dummy(demo_2015)
-
-# write.csv(demo_2015, file = file.path(output_dir, "demo_2015.csv"),
-#           row.names = FALSE)
+write.csv(demo_2015, file = file.path(output_dir, "demo_2015.csv"),
+          row.names = FALSE)
 
 PAQ_2015 <- download_org_nhanes("Q", "PAQ_I")
-
+raw_features_used <- save_features(raw_features_used, "PAQ_I", colnames(PAQ_2015))
 PAQ_2015 <- convert_dummy(PAQ_2015)
 
 RXQ_2015 <- download_org_nhanes("Q", "RXQ_RX_I")
@@ -121,6 +135,7 @@ d1_24supp_2015 <- download_org_nhanes("DIET", "DS1IDS_I")
 d1_diet_2015 <- download_org_nhanes("DIET", "DR1IFF_I")
 
 d1_diet_2015 <- d1_diet_2015[, !sapply(d1_diet_2015, is.factor)]
+raw_features_used <- save_features(raw_features_used, "DR1IFF_I", colnames(d1_diet_2015))
 d1_diet_2015$`USDA food code` <- food_codes$category_description[match(d1_diet_2015$`USDA food code`, food_codes$food_code)]
 
 dir.create(file.path(output_dir), showWarnings = FALSE)
@@ -146,13 +161,16 @@ nhanesA::nhanesTables(data_group="LAB", year=2015)
 #### Small amount data cleaning of d1_diet_2015 and BP_2015 datasets ####
 #reogranize with dummy variables to work with RF
 
-d1_diet_2015[, !sapply(d1_diet_2015, is.factor)]
+# d1_diet_2015[, !sapply(d1_diet_2015, is.factor)]
 
-d1_diet_2015 <- fastDummies::dummy_cols(d1_diet_2015, select_columns = c("USDA food code"),
-                                     ignore_na = TRUE, remove_selected_columns = TRUE)
-
+d1_diet_2015 <- fastDummies::dummy_cols(d1_diet_2015,
+                                        select_columns = c("USDA food code"),
+                                        ignore_na = TRUE, remove_selected_columns = TRUE)
+d1_diet_2015 <- subset(d1_diet_2015, select = -c(2:8)) #Row sums on these doesn't make sense
 d1_diet_2015 <- rowsum(d1_diet_2015[,3:ncol(d1_diet_2015)],
-                    group = d1_diet_2015$`Respondent sequence number.`,na.rm=T, )
+               group = d1_diet_2015$`Respondent sequence number.`,na.rm=T, )
+# d1_diet_2015 <- rowsum(d1_diet_2015[,3:ncol(d1_diet_2015)],
+#                     group = d1_diet_2015$`Respondent sequence number.`,na.rm=T, )
 
 #rowsum removes group column and we need it downstream, so put it back:
 d1_diet_2015$`Respondent sequence number.` <- row.names(d1_diet_2015)
@@ -213,9 +231,32 @@ PAQ_demo_d1_diet_2015 <- PAQ_demo_d1_diet_2015[, !sapply(PAQ_demo_d1_diet_2015, 
 write.csv(PAQ_demo_d1_diet_2015, file = file.path(output_dir, "PAQ_noRx_demo_d1_diet_2015_match_bp_SYST1_RF.csv"),
           row.names = FALSE)
 
+#### Add Phthalates and Plasticizers Metabolites - Urine (PHTHTE_I) ####
+nhanesA::nhanesTables(data_group="LAB", year=2015)
+PHTHTE_2015 <- download_org_nhanes("LAB","PHTHTE_I")
+raw_features_used <- save_features(raw_features_used, "PHTHTE_I", colnames(PHTHTE_2015))
+PHTHTE_2015 <- convert_dummy(PHTHTE_2015)
 
+write.csv(PHTHTE_2015, file = file.path(output_dir, "PHTHTE_2015.csv"),
+          row.names = FALSE)
+my_intersect <- intersect(PHTHTE_2015$`Respondent sequence number.`, my_intersect)
 
+d1_diet_2015 <- d1_diet_2015[d1_diet_2015$`Respondent sequence number.`  %in% my_intersect,]
+BP_2015 <- BP_2015[BP_2015$`Respondent sequence number.` %in% my_intersect,]
+demo_2015 <- demo_2015[demo_2015$`Respondent sequence number.`  %in% my_intersect,]
 
+write.csv(d1_diet_2015, file = file.path(output_dir, "PHTHTE_PAQ_noRx_d1_diet_2015_match_bp_SYST1_RF.csv"),
+          row.names = FALSE)
+write.csv(demo_2015, file = file.path(output_dir, "PHTHTE_PAQ_noRx_demo_2015_match_bp_SYST1_RF.csv"),
+          row.names = FALSE)
+write.csv(BP_2015, file = file.path(output_dir, "PHTHTE_PAQ_noRx_BP_2015_match_diet_SYST1_RF.csv"),
+          row.names = FALSE)
+PHTHTE_PAQ_demo_d1_diet_2015 <- merge(PHTHTE_2015, PAQ_demo_d1_diet_2015, by = "Respondent sequence number." )
+PHTHTE_PAQ_demo_d1_diet_2015 <- PHTHTE_PAQ_demo_d1_diet_2015[, !sapply(PAQ_demo_d1_diet_2015, is.factor)]
+write.csv(PHTHTE_PAQ_demo_d1_diet_2015, file = file.path(output_dir, "PHTHTE_PAQ_noRx_demo_d1_diet_2015_match_bp_SYST1_RF.csv"),
+          row.names = FALSE)
+
+write.csv(raw_features_used, file = file.path(output_dir,"graphics", "13_Feb_2024", "raw_predictor_features.csv"))
 #Short version for testing ML code for completion
 short_rsn <- my_intersect[1:200]
 sh_d1_diet_2015 <- d1_diet_2015[d1_diet_2015$`Respondent sequence number.`  %in% short_rsn,]
@@ -231,62 +272,62 @@ write.csv(sh_BP_2015, file = file.path(output_dir, "short_BP_2015_match_diet_SYS
 
 
 #### Create "foods only" dataset ####
-d1_diet_2015 <- nhanesA::nhanes("DR1IFF_I")
-
-d1_diet_2015_vars <- nhanesA::nhanesTableVars(data_group = "DIET",
-                                           nh_table = "DR1IFF_I",
-                                           namesonly = TRUE)
-d1_diet_2015 <- nhanesA::nhanesTranslate(nh_table = "DR1IFF_I",
-                                      d1_diet_2015_vars, data = d1_diet_2015)
-d1_diet_2015 <- nhanes_names(d1_diet_2015, "DIET", "DR1IFF_I")
-
-d1_diet_2015 <- d1_diet_2015[, !sapply(d1_diet_2015, is.factor)]
-d1_diet_2015$`USDA food code` <- food_codes$food_code_description[match(d1_diet_2015$`USDA food code`, food_codes$food_code)]
-
-dir.create(file.path(output_dir), showWarnings = FALSE)
-write.csv(d1_diet_2015, file = file.path(output_dir, "d1_diet_2015.csv"),
-          row.names = FALSE)
-
-nhanesA::nhanesTables(data_group="EXAM", year=2015)
-BP_2015 <- nhanesA::nhanes("BPX_I")
-
-BP_2015_vars <- nhanesA::nhanesTableVars(data_group = "EXAM",
-                                         nh_table = "BPX_I",
-                                         namesonly = TRUE)
-BP_2015 <- nhanesA::nhanesTranslate(nh_table = "BPX_I",
-                                    BP_2015_vars, data = BP_2015)
-
-BP_2015 <- nhanes_names(BP_2015, "EXAM", "BPX_I")
-
-write.csv(BP_2015, file = file.path(output_dir, "BP_2015.csv"),
-          row.names = FALSE)
-
-nhanesA::nhanesTables(data_group="LAB", year=2015)
-
-#### Small amount data cleaning of d1_diet_2015 and BP_2015 datasets ####
-#reogranize with dummy variables to work with RF
-
-d1_diet_2015[, !sapply(d1_diet_2015, is.factor)]
-
-d1_diet_2015 <- fastDummies::dummy_cols(d1_diet_2015, select_columns = c("USDA food code"),
-                                     ignore_na = TRUE, remove_selected_columns = TRUE)
-
-d1_diet_2015 <- rowsum(d1_diet_2015[,3:ncol(d1_diet_2015)],
-                    group = d1_diet_2015$`Respondent sequence number.`,na.rm=T, )
-
-#rowsum removes group column and we need it downstream, so put it back:
-d1_diet_2015$`Respondent sequence number.` <- row.names(d1_diet_2015)
-
-
-# Create datasets from interection of BP and Diet datasets
-BP_2015 <- BP_2015[!is.na(BP_2015$`Systolic:  Blood pressure (first reading) mm Hg`),]
-my_intersect <- intersect(BP_2015$`Respondent sequence number.`,
-                          d1_diet_2015$`Respondent sequence number.`)
-
-d1_diet_2015 <- d1_diet_2015[d1_diet_2015$`Respondent sequence number.`  %in% my_intersect,]
-BP_2015 <- BP_2015[BP_2015$`Respondent sequence number.` %in% my_intersect,]
-demo_2015 <- demo_2015[demo_2015$`Respondent sequence number.`  %in% my_intersect,]
-
-write.csv(d1_diet_2015, file = file.path(output_dir, "d1_diet_2015_match_bp_SYST1_RF.csv"),
-          row.names = FALSE)
-
+# d1_diet_2015 <- nhanesA::nhanes("DR1IFF_I")
+# 
+# d1_diet_2015_vars <- nhanesA::nhanesTableVars(data_group = "DIET",
+#                                            nh_table = "DR1IFF_I",
+#                                            namesonly = TRUE)
+# d1_diet_2015 <- nhanesA::nhanesTranslate(nh_table = "DR1IFF_I",
+#                                       d1_diet_2015_vars, data = d1_diet_2015)
+# d1_diet_2015 <- nhanes_names(d1_diet_2015, "DIET", "DR1IFF_I")
+# 
+# d1_diet_2015 <- d1_diet_2015[, !sapply(d1_diet_2015, is.factor)]
+# d1_diet_2015$`USDA food code` <- food_codes$food_code_description[match(d1_diet_2015$`USDA food code`, food_codes$food_code)]
+# 
+# dir.create(file.path(output_dir), showWarnings = FALSE)
+# write.csv(d1_diet_2015, file = file.path(output_dir, "d1_diet_2015.csv"),
+#           row.names = FALSE)
+# 
+# nhanesA::nhanesTables(data_group="EXAM", year=2015)
+# BP_2015 <- nhanesA::nhanes("BPX_I")
+# 
+# BP_2015_vars <- nhanesA::nhanesTableVars(data_group = "EXAM",
+#                                          nh_table = "BPX_I",
+#                                          namesonly = TRUE)
+# BP_2015 <- nhanesA::nhanesTranslate(nh_table = "BPX_I",
+#                                     BP_2015_vars, data = BP_2015)
+# 
+# BP_2015 <- nhanes_names(BP_2015, "EXAM", "BPX_I")
+# 
+# write.csv(BP_2015, file = file.path(output_dir, "BP_2015.csv"),
+#           row.names = FALSE)
+# 
+# nhanesA::nhanesTables(data_group="LAB", year=2015)
+# 
+# #### Small amount data cleaning of d1_diet_2015 and BP_2015 datasets ####
+# #reogranize with dummy variables to work with RF
+# 
+# d1_diet_2015[, !sapply(d1_diet_2015, is.factor)]
+# 
+# d1_diet_2015 <- fastDummies::dummy_cols(d1_diet_2015, select_columns = c("USDA food code"),
+#                                      ignore_na = TRUE, remove_selected_columns = TRUE)
+# 
+# d1_diet_2015 <- rowsum(d1_diet_2015[,3:ncol(d1_diet_2015)],
+#                     group = d1_diet_2015$`Respondent sequence number.`,na.rm=T, )
+# 
+# #rowsum removes group column and we need it downstream, so put it back:
+# d1_diet_2015$`Respondent sequence number.` <- row.names(d1_diet_2015)
+# 
+# 
+# # Create datasets from interection of BP and Diet datasets
+# BP_2015 <- BP_2015[!is.na(BP_2015$`Systolic:  Blood pressure (first reading) mm Hg`),]
+# my_intersect <- intersect(BP_2015$`Respondent sequence number.`,
+#                           d1_diet_2015$`Respondent sequence number.`)
+# 
+# d1_diet_2015 <- d1_diet_2015[d1_diet_2015$`Respondent sequence number.`  %in% my_intersect,]
+# BP_2015 <- BP_2015[BP_2015$`Respondent sequence number.` %in% my_intersect,]
+# demo_2015 <- demo_2015[demo_2015$`Respondent sequence number.`  %in% my_intersect,]
+# 
+# write.csv(d1_diet_2015, file = file.path(output_dir, "d1_diet_2015_match_bp_SYST1_RF.csv"),
+#           row.names = FALSE)
+# 
