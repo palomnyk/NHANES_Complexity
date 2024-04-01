bb8d329a7bd7d3a466f7b85957782abfcc1b88c0
palomnyk <aaronyerke@gmail.com>
2024-04-01 11:33:39 -0500

    Add day2 dietary data.

--- lib/scripts/data_org/import_org_diet.R
+++ lib/scripts/data_org/import_org_diet.R
@@ -1,3 +1,5 @@
+
+#keep only adults
 # Author: Aaron Yerke (aaronyerke@gmail.com)
 # Script for importing and organizing dietary data
 # https://cran.r-project.org/web/packages/nhanesA/vignettes/Introducing_nhanesA.html
@@ -61,38 +63,51 @@ d1_diet_2015 <- nhanesA::nhanesTranslate(nh_table = "DR1IFF_I",
                                   colnames = names(d1_diet_2015))
 d1_diet_2015 <- nhanes_names(d1_diet_2015,"DIET", "DR1IFF_I")
 attr(d1_diet_2015, "names") <- sub("[[:punct:]]$", "", names(d1_diet_2015))
-#keep only adults
-d1_diet_2015 <- d1_diet_2015[d1_diet_2015[,id_var] %in% adult_seqn, ]
-d1_diet_2015 <- type.convert(d1_diet_2015, as.is = TRUE)#Reset column types automatically (for factors)
+# Day 2 diet
+d2_diet_2015 <- nhanesA::nhanes("DR1IFF_I")
+d2_diet_2015 <- nhanesA::nhanesTranslate(nh_table = "DR2IFF_I",
+                                         data = d2_diet_2015,
+                                         details = TRUE,
+                                         colnames = names(d2_diet_2015))
+d2_diet_2015 <- nhanes_names(d2_diet_2015,"DIET", "DR1IFF_I")
+attr(d2_diet_2015, "names") <- sub("[[:punct:]]$", "", names(d2_diet_2015))
 
-nutr_vars <- c(20:ncol(d1_diet_2015))
-nutr_d1_diet_2015 <- rowsum(d1_diet_2015[,nutr_vars],
-                       group = d1_diet_2015[,id_var], na.rm=T)
-nutr_d1_diet_2015[,id_var] <- row.names(nutr_d1_diet_2015)
+#combine d1 and d2
+d1d2_diet_2015 <- rbind(d1_diet_2015, d2_diet_2015)
+
+#keep only adults
+d1d2_diet_2015 <- d1d2_diet_2015[d1d2_diet_2015[,id_var] %in% adult_seqn, ]
+d1d2_diet_2015 <- type.convert(d1d2_diet_2015, as.is = TRUE)#Reset column types automatically (for factors)
+
+#organize nutrient variables first
+nutr_vars <- c(20:ncol(d1d2_diet_2015))
+nutr_d1d2_diet_2015 <- rowsum(d1d2_diet_2015[,nutr_vars],
+                       group = d1d2_diet_2015[,id_var], na.rm=T)
+nutr_d1d2_diet_2015[,id_var] <- row.names(nutr_d1d2_diet_2015)
 
 dir.create(file.path(output_dir), showWarnings = FALSE)
-write.csv(nutr_d1_diet_2015, file = file.path(output_dir, "d1_nutr_only_2015.csv"),
+write.csv(nutr_d1d2_diet_2015, file = file.path(output_dir, "d1d2_nutr_only_2015.csv"),
           row.names = FALSE)
-# d1_diet_2015 <- d1_diet_2015[, !sapply(d1_diet_2015, is.factor)]
+# d1d2_diet_2015 <- d1d2_diet_2015[, !sapply(d1d2_diet_2015, is.factor)]
 #Save usda food codes for overwriting
-orig_fd_code <- d1_diet_2015$`USDA food code`
+orig_fd_code <- d1d2_diet_2015$`USDA food code`
 #Make food grams dummys
-d1_diet_2015$`USDA food code` <- food_codes$food_code_description[match(orig_fd_code, food_codes$food_code)]
+d1d2_diet_2015$`USDA food code` <- food_codes$food_code_description[match(orig_fd_code, food_codes$food_code)]
 
-USDA_food_g_only <- two_column_dummy(d1_diet_2015,
+USDA_food_g_only <- two_column_dummy(d1d2_diet_2015,
                     id_colnm = "Respondent sequence number",
                     item_colnm = "USDA food code",
                     count_colnm = "Gram weight of the food/individual component")
 print(paste("Number of adult diet participants:", nrow(USDA_food_g_only)))
 
-write.csv(USDA_food_g_only, file = file.path(output_dir, "d1_food_g_2015.csv"),
+write.csv(USDA_food_g_only, file = file.path(output_dir, "d1d2_food_g_2015.csv"),
           row.names = FALSE)
 
-nutri_food_g <- merge(USDA_food_g_only, nutr_d1_diet_2015, by = id_var)
-write.csv(nutri_food_g, file = file.path(output_dir, "d1_nutri_food_g_2015.csv"),
+nutri_food_g <- merge(USDA_food_g_only, nutr_d1d2_diet_2015, by = id_var)
+write.csv(nutri_food_g, file = file.path(output_dir, "d1d2_nutri_food_g_2015.csv"),
           row.names = FALSE)
 
-USDA_food_simple <- subset(d1_diet_2015, select = c("USDA food code", id_var))
+USDA_food_simple <- subset(d1d2_diet_2015, select = c("USDA food code", id_var))
 
 USDA_food_simple <- fastDummies::dummy_cols(USDA_food_simple,
                                               select_columns = c("USDA food code"),
@@ -102,32 +117,32 @@ USDA_food_simple <- rowsum(USDA_food_simple,
                              group = USDA_food_simple[,id_var],na.rm=T, )
 USDA_food_simple[,id_var] <- row.names(USDA_food_simple)
 
-write.csv(USDA_food_simple, file = file.path(output_dir, "d1_food_2015.csv"),
+write.csv(USDA_food_simple, file = file.path(output_dir, "d1d2_food_2015.csv"),
           row.names = FALSE)
 
-nutri_food_simple <- merge(USDA_food_simple, nutr_d1_diet_2015, by = id_var)
-write.csv(nutri_food_simple, file = file.path(output_dir, "d1_nutri_food_2015.csv"),
+nutri_food_simple <- merge(USDA_food_simple, nutr_d1d2_diet_2015, by = id_var)
+write.csv(nutri_food_simple, file = file.path(output_dir, "d1d2_nutri_food_2015.csv"),
           row.names = FALSE)
 
 # d1_tot_diet_2015 <- download_org_nhanes(dt_group = "DIET", nh_tble = "DR1TOT_I")
 
 #### Food categories ####
-d1_diet_2015$`USDA food code` <- food_codes$category_description[match(orig_fd_code, food_codes$food_code)]
+d1d2_diet_2015$`USDA food code` <- food_codes$category_description[match(orig_fd_code, food_codes$food_code)]
 
-cat_g_only <- two_column_dummy(d1_diet_2015,
+cat_g_only <- two_column_dummy(d1d2_diet_2015,
                                      id_colnm = "Respondent sequence number",
                                      item_colnm = "USDA food code",
                                      count_colnm = "Gram weight of the food/individual component")
 print(paste("Number of adult diet participants:", nrow(cat_g_only)))
 
-write.csv(cat_g_only, file = file.path(output_dir, "d1_cat_g_2015.csv"),
+write.csv(cat_g_only, file = file.path(output_dir, "d1d2_cat_g_2015.csv"),
           row.names = FALSE)
 
-nutri_food_g <- merge(cat_g_only, nutr_d1_diet_2015, by = id_var)
-write.csv(nutri_food_g, file = file.path(output_dir, "d1_nutri_cat_g_2015.csv"),
+nutri_food_g <- merge(cat_g_only, nutr_d1d2_diet_2015, by = id_var)
+write.csv(nutri_food_g, file = file.path(output_dir, "d1d2_nutri_cat_g_2015.csv"),
           row.names = FALSE)
 
-cat_simple <- subset(d1_diet_2015, select = c("USDA food code", id_var))
+cat_simple <- subset(d1d2_diet_2015, select = c("USDA food code", id_var))
 
 cat_simple <- fastDummies::dummy_cols(cat_simple,
                                             select_columns = c("USDA food code"),
@@ -137,17 +152,17 @@ cat_simple <- rowsum(cat_simple,
                            group = cat_simple[,id_var],na.rm=T, )
 cat_simple[,id_var] <- row.names(cat_simple)
 
-write.csv(cat_simple, file = file.path(output_dir, "d1_cat_2015.csv"),
+write.csv(cat_simple, file = file.path(output_dir, "d1d2_cat_2015.csv"),
           row.names = FALSE)
 
-nutri_food_simple <- merge(cat_simple, nutr_d1_diet_2015, by = id_var)
-write.csv(nutri_food_simple, file = file.path(output_dir, "d1_nutri_cat_2015.csv"),
+nutri_food_simple <- merge(cat_simple, nutr_d1d2_diet_2015, by = id_var)
+write.csv(nutri_food_simple, file = file.path(output_dir, "d1d2_nutri_cat_2015.csv"),
           row.names = FALSE)
 # G. Category grams + food grams + nutrient (10)
 nutri_food_g_cat_g <- merge(nutri_food_g, USDA_food_g_only)
-write.csv(nutri_food_g_cat_g, file = file.path(output_dir, "d1_nutri_food_g_cat_g_2015.csv"),
+write.csv(nutri_food_g_cat_g, file = file.path(output_dir, "d1d2_nutri_food_g_cat_g_2015.csv"),
           row.names = FALSE)
 # H. Category simple + food simple + nutrient (11)
 nutri_food_cat <- merge(nutri_food_simple, USDA_food_simple)
-write.csv(nutri_food_cat, file = file.path(output_dir, "d1_nutri_food_cat_2015.csv"),
+write.csv(nutri_food_cat, file = file.path(output_dir, "d1d2_nutri_food_cat_2015.csv"),
           row.names = FALSE)
