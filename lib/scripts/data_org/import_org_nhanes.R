cc11d3f74518785222b40b712f3a0374fef5b7c2
palomnyk <aaronyerke@gmail.com>
2024-05-23 09:32:57 -0500

    Add checks for feature covariance and print statements for documentation.

--- lib/scripts/data_org/import_org_nhanes.R
+++ lib/scripts/data_org/import_org_nhanes.R
@@ -8,6 +8,8 @@ rm(list = ls()) #clear workspace
 if (!requireNamespace("BiocManager", quietly = TRUE)) install.packages("BiocManager")
 if (!requireNamespace("nhanesA", quietly = TRUE)) BiocManager::install("nhanesA")
 library("nhanesA")
+if (!requireNamespace("reshape2", quietly = TRUE)) BiocManager::install("reshape2")
+library("reshape2")
 
 print("Loaded dependencies")
 source(file.path("lib", "scripts","data_org", "data_org_func.R"))
@@ -16,10 +18,12 @@ source(file.path("lib", "scripts","data_org", "data_org_func.R"))
 output_dir <- file.path("Data", "nhanesA_tables")
 dir.create(output_dir)
 id_var <- "Respondent sequence number"
+cor_thresh <- 0.9
 
 #### Loading in data ####
 import_tables <- read.csv(file.path("Data", "nhanesTablesToAdd.csv"),
-                           header = T, sep = ",", comment.char = "#")
+                          header = T, sep = ",", comment.char = "#")
+print(table(import_tables$data_group))
 short_col_name <- c()
 long_col_name <- c()
 dt_grp_name <- c()
@@ -50,10 +54,11 @@ for (i in 1:nrow(import_tables)){
   nh_table_name <- c(nh_table_name, rep(nh_tbl, num_col))
   year <- c(year, rep(yr, num_col))
   
+  #TODO: Drop columns listed in nhanesTablesToAdd.csv (drop_col)
   dl_tble <- nhanesA::nhanesTranslate(nh_table = nh_tbl,
-                           data = dl_tble,
-                           details = TRUE,
-                           colnames = names(dl_tble))
+                                      data = dl_tble,
+                                      details = TRUE,
+                                      colnames = names(dl_tble))
   
   dl_tble <- nhanes_names(dl_tble, dt_grp, nh_tbl)
   
@@ -78,15 +83,46 @@ for (i in 1:nrow(import_tables)){
   }
 }
 
-full_df <- full_df[vapply(full_df, function(x) length(unique(x)) > 1, logical(1L))]
+codebook <- data.frame(short_col_name,
+                       long_col_name,
+                       dt_grp_name,
+                       nh_table_name,
+                       year)
+
+write.csv(codebook, file = file.path(output_dir, "comb_NHANES_summary.csv"),
+          row.names = FALSE)
+
+no_var <- vapply(full_df, function(x) length(unique(x)) == 1, logical(1L))
+print(paste("Num dropped features with no variance:", sum(no_var)))
+full_df <- full_df[vapply(full_df, function(x) length(unique(x)) > 1, logical(1L))]#remove columns with no variance
 row.names(full_df) <- full_df[,id_var]
+
+print(paste("B4 DUM NCOL, NROW, %NA", ncol(full_df), nrow(full_df), mean(is.na(full_df))))
+
 full_df <- convert_dummy(full_df)
+print(paste("AFT DUM NCOL, NROW, %NA", ncol(full_df), nrow(full_df), mean(is.na(full_df))))
 
 print(paste("Any NA in respondent ID:", any(is.na(full_df[,id_var]))))
 stopifnot(any(!is.na(full_df[,id_var])))
 print(paste("Respondent IDs are unique:",
             length(unique(full_df[,id_var])) == nrow(full_df)))
 stopifnot(length(unique(full_df[,id_var])) == nrow(full_df))
+
+#### Create correlation matrix, remove cor features #####
+cor_matrx <- cor(full_df)
+
+upper_tri <- cor_matrx
+upper_tri[lower.tri(upper_tri)] <- NA
+
+melted_cormat <- melt(upper_tri, na.rm = TRUE)
+
+over_cor <- subset(melted_cormat, melted_cormat$Var1 != melted_cormat$Var2)
+over_cor <- subset(over_cor, value > cor_thresh)
+
+write.csv(over_cor, file = file.path(output_dir, paste0("over_cor_",cor_thresh,"_combined_NHANES_dum.csv")),
+          row.names = FALSE)
+
+full_df <- full_df[, !colnames(full_df) %in% over_cor$Var2] #drop highly correlated variables
 
 write.csv(full_df, file = file.path(output_dir, "combined_NHANES_dum.csv"),
           row.names = FALSE)
