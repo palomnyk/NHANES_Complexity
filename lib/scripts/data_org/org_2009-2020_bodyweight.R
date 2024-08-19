87e8ca52c53ee9a5b8485c02e8cb332b6375246e
palomnyk <aaronyerke@gmail.com>
2024-08-19 18:14:53 -0500

    checkpoint to move back local

---
+++ lib/scripts/data_org/org_2009-2020_bodyweight.R
@@ -0,0 +1,67 @@
+# Author: Aaron Yerke (aaronyerke@gmail.com)
+# Script for averaging and organizing demographic data for the 2009-2020
+
+rm(list = ls()) #clear workspace
+
+#### Loading dependencies ####
+if (!requireNamespace("BiocManager", quietly = TRUE)) install.packages("BiocManager")
+if (!requireNamespace("nhanesA", quietly = TRUE)) BiocManager::install("nhanesA")
+library("nhanesA")
+
+#### Load functions ####
+source(file.path("lib", "scripts","data_org", "data_org_func.R"))
+
+# --------------------------------------------------------------------------
+print("Establishing directory layout and other constants.")
+# --------------------------------------------------------------------------
+#### Establish directory layout and other constants ####
+output_dir <- file.path("Data", "exam")
+dir.create(output_dir)
+
+id_name <- "Respondent sequence number"
+#### Loading in data ####
+import_tables <- read.csv(file = file.path("lib", "datasets", "body_weight_2009-2020.csv"),
+                          header = T, comment.char = "#",
+                          check.names =F)
+
+import_cols <- c(unique(import_tables$column_short_name),"SEQN")
+target_names <- c(unique(import_tables$our_name),id_name)
+my_cols <- c(target_names, "year", "nh_table_name", id_name)
+
+full_table <- data.frame()
+
+uniq_tables <- unique(import_tables$nh_table)
+#### Build tables ####
+for (ut in 1:length(uniq_tables)){
+  uniq_table <- uniq_tables[ut]
+  subtable <- import_tables[import_tables$nh_table == uniq_table, ]
+  target_cols <- c(unique(subtable$column_short_name), "SEQN")
+  yr <- subtable$year_start[1]
+  dt_grp <- subtable$data_group[1]
+  nh_tbl <- subtable$nh_table[1]
+  fname <- paste0(nh_tbl,"_", yr, ".csv")
+  print(fname)
+  fname_path <- file.path(output_dir, fname)
+  if (!file.exists(fname_path)){
+    print(paste(fname_path, "not found - Downloading!"))
+    dl_tble <- nhanesA::nhanes(nh_tbl)
+    write.csv(dl_tble, fname_path, row.names = FALSE)
+    Sys.sleep(2)
+  }else{
+    dl_tble <- read.csv(fname_path, header = TRUE, check.names = FALSE)
+  }
+  dl_tble <- dl_tble[,sapply(dl_tble, function(x) !all(is.na(x)))]
+  dl_tble <- dl_tble[,import_cols]
+  names(dl_tble) <- target_names[match(names(dl_tble), import_cols)]
+  
+  dl_tble$nh_table <- rep(nh_tbl, nrow(dl_tble))
+  dl_tble$year <- rep(yr, nrow(dl_tble))
+  
+  if (ncol(full_table) == 0) full_table <- dl_tble
+  else full_table <- rbind(full_table, dl_tble)
+}
+
+write.csv(full_table, file = file.path(output_dir, "bodyweight_2009-2020.csv"),
+          row.names = FALSE)
+
+print("End R script.")
--- lib/scripts/descriptive/feature_correlations.R
+++ lib/scripts/descriptive/feature_correlations.R
@@ -19,11 +19,32 @@ print("Loaded dependencies")
 source(file.path("lib", "scripts","data_org", "data_org_func.R"))
 
 #### Functions ####
+create_plot <- function(pred_df, pred_colnam, resp_df,resp_colnm, id_var,
+                        title_group = "title") {
+  shared_seqs <- intersect(pred_df[!is.na(pred_df[,"Sodium (mg"]),id_var],
+                           resp_df[!is.na(resp_df[,"Systolic_mean"]),id_var])
+  my_cor <- cor(pred_df[pred_df[,id_var] %in% shared_seqs,"Sodium (mg"], 
+                resp_df[resp_df[,id_var] %in% shared_seqs,"Systolic_mean"])
+  
+  plot(pred_df[pred_df[,id_var] %in% shared_seqs,"Sodium (mg"], 
+       resp_df[resp_df[,id_var] %in% shared_seqs,"Systolic_mean"],
+       main = paste0(title_group, ", cor: ", round(my_cor,3)),
+       xlab = pred_colnam,
+       ylab = resp_colnm,
+       cex = 0.1)
+  # return(my_cor)
+}
 
 #### Parse commandline arguements ####
 option_list <- list(
   optparse::make_option(c("-o", "--out_subdir"), type="character", 
                         default=file.path("diet_test"), 
+                        help="dataset dir path"),
+  optparse::make_option(c("-c", "--pred_col"), type="character", 
+                        default="Sodium (mg", 
+                        help="dataset dir path"),
+  optparse::make_option(c("-r", "--resp_df"), type="character", 
+                        default="Data/respns_vars/2009-2020cardio_respns_vars.csv", 
                         help="dataset dir path")
 );
 opt_parser <- optparse::OptionParser(option_list=option_list);
@@ -41,23 +62,97 @@ numeric_only <- c("Total Cholesterol (mg/dL","Triglyceride (mg/dL",
 id_var <- "Respondent sequence number"
 
 #### Loading in data ####
-demo_2009_2020 <- read.csv("Data/demo/demo_2009-2020.csv", header = TRUE)
+demo_2009_2020 <- read.csv("Data/demo/demo_2009-2020.csv", header = TRUE,
+                           check.names = FALSE)
 diet_df <- read.csv("Data/diet/multi_year/d1d2_nutr_only_2009-2020.csv",
-                     header = TRUE, check.names = FALSE)
-resp_df <- read.csv("Data/respns_vars/2009-2020cardio_respns_vars.csv",
-                    header = TRUE,check.names = FALSE)
+                    header = TRUE, check.names = FALSE)
+cardio_df <- read.csv(opt$resp_df, header = TRUE,check.names = FALSE)
 
-#### Plot BP vs Sodium ####
-shared_seqs <- intersect(diet_df[!is.na(diet_df[,"Sodium (mg"]),id_var],
-                         resp_df[!is.na(resp_df[,"Systolic_mean"]),id_var])
-plot(diet_df[diet_df[,id_var] %in% shared_seqs,"Sodium (mg"], 
-     resp_df[resp_df[,id_var] %in% shared_seqs,"Systolic_mean"])
-cor(diet_df[diet_df[,id_var] %in% shared_seqs,"Sodium (mg"], 
-    resp_df[resp_df[,id_var] %in% shared_seqs,"Systolic_mean"])
+#### Create variables to cycle through ####
+age_low_range <- seq(from = 18, to = 80, by = 16)
+age_upper_ranges <- age_low_range + 16
+genders <- unique(demo_2009_2020[,"gender"])
+ethnicities <- unique(demo_2009_2020[,"ethnicity"])
+num_rows <- length(ethnicities) * length(age_low_range)
 
-male_subdf <- demo_2009_2020[demo_2009_2020$gender == "Male" &
-                             demo_2009_2020$age >= 18,]
-male_age_quantile_size <- (max(male_subdf$age) - 18)/4
+base_fn <- basename(opt$resp_df)
 
+pdf(file = file.path(output_dir, "graphics", paste0(base_fn,"_VS_",opt$pred_col, ".pdf")))
+for (r in 1:ncol(cardio_df)) {
+  resp_col <- names(cardio_df)[r]
+  for (g in 1:length(genders)){
+    # Create data to fill in
+    age_range_ord <- character(length = length(age_low_range))#for order in factor level
+    cor_table <- data.frame(age_range = character(length = num_rows),
+                            ethnicities = character(length = num_rows),
+                            pearson = numeric(length = num_rows))
+    row_count <- 1
+    for (a in 1:length(age_low_range)){
+      age_range <- paste0(age_low_range[a], "-", as.integer(age_upper_ranges[a])-1)
+      age_range_ord[a] <- age_range
+      for (e in 1:length(ethnicities)){
+        sub_demo <- demo_2009_2020[demo_2009_2020$gender == genders[g] &
+                                     demo_2009_2020$ethnicity == ethnicities[e] &
+                                     demo_2009_2020$age >= age_low_range[a] &
+                                     demo_2009_2020$age < age_upper_ranges[a],]
+        age_range_ord[a] <- paste0(age_low_range[a], "-", age_upper_ranges[a])
+        shared_seqs <- intersect(diet_df[!is.na(diet_df[,]),id_var],
+                                 cardio_df[!is.na(cardio_df[,resp_col]),id_var])
+        shared_seqs <- intersect(shared_seqs, sub_demo$`Respondent sequence number`)
+        my_cor <- cor(diet_df[diet_df[,id_var] %in% shared_seqs,opt$pred_col], 
+                      cardio_df[cardio_df[,id_var] %in% shared_seqs,resp_col],
+                      method = "kendall")
+        
+        cor_table$age_range[row_count] <- age_range
+        cor_table$ethnicities[row_count] <- ethnicities[e]
+        cor_table$pearson[row_count] <- my_cor^2
+        row_count <- row_count + 1
+      }# for e
+    }# for a
+    # cor_table$age_range <- factor(cor_table$age_range, levels = age_range_ord)
+    p <- ggplot2::ggplot(cor_table, aes(x = ethnicities, y=pearson, fill = age_range)) +
+      ggplot2::geom_col(position="dodge", width=0.85, color="black") +
+      ggplot2::ggtitle(paste0(genders[g], ": ", resp_col, " VS ", opt$pred_col)) +
+      ggplot2::xlab("Age range") +
+      ggplot2::ylab(expression(paste("Kendall R"^"2"))) + #create superscript
+      ggplot2::theme_minimal() +
+      ggplot2::theme(axis.line = element_line(color="black"),
+                     axis.ticks = element_line(color="black"),
+                     panel.border = element_blank(),
+                     axis.text.x = element_text(angle = 45, hjust=1),
+                     text=element_text(size=15), #change font size of all text
+                     axis.text=element_text(size=15), #change font size of axis text
+                     axis.title=element_text(size=17), #change font size of axis titles
+                     plot.title=element_text(size=17), #change font size of plot title
+                     legend.text=element_text(size=15), #change font size of legend text
+                     legend.title=element_text(size=16)) #change font size of legend title
+    print(p)
+  }# for g
+}#r
+dev.off()
+create_plot(diet_df, opt$pred_col, cardio_df, "Systolic_mean", id_var, "No subsets")
 
+# sub_demo <- demo_2009_2020[demo_2009_2020$gender == "Male" &
+#                              demo_2009_2020$age >= 18,]
+# sub_diet <- diet_df[diet_df[,id_var] %in% sub_demo$`Respondent sequence number`,]
+# create_plot(sub_diet, opt$pred_col, cardio_df, "Systolic_mean", id_var, 
+#             "All male adults")
+# 
+# sub_demo <- demo_2009_2020[demo_2009_2020$gender == "Female" &
+#                              demo_2009_2020$age >= 18,]
+# sub_diet <- diet_df[diet_df[,id_var] %in% sub_demo$`Respondent sequence number`,]
+# create_plot(sub_diet, opt$pred_col, cardio_df, "Systolic_mean", id_var, 
+#             "All female adults")
+# 
+# sub_demo <- demo_2009_2020[demo_2009_2020$gender == "Female" &
+#                              demo_2009_2020$age >= 60,]
+# sub_diet <- diet_df[diet_df[,id_var] %in% sub_demo$`Respondent sequence number`,]
+# create_plot(sub_diet, opt$pred_col, cardio_df, "Systolic_mean", id_var, 
+#             "Females 60 and older")
+# 
+# sub_demo <- demo_2009_2020[demo_2009_2020$gender == "Male" &
+#                              demo_2009_2020$age >= 70,]
+# sub_diet <- diet_df[diet_df[,id_var] %in% sub_demo$`Respondent sequence number`,]
+# create_plot(sub_diet, opt$pred_col, cardio_df, "Systolic_mean", id_var, 
+#             "Males 70 and older")
 print("End of R script!")
