6d08cf76d7f6432ef6072a8718e2c6459bddf8ec
palomnyk <aaronyerke@gmail.com>
2024-10-08 11:35:14 -0500

    update to compare day1_only or day1_plus_day_2

--- lib/scripts/fig/diet_org_day_compare.R
+++ lib/scripts/fig/diet_org_day_compare.R
@@ -1,6 +1,9 @@
 # Author: Aaron Yerke (aaronyerke@gmail.com)
 # Script for making graphic that compares the various methods of organizing
 # diet data.
+# Also compares d1 vs d1d2,
+#   food vs cat/nutrition,
+#   for each response variable.
 
 rm(list = ls()) #clear workspace
 chooseCRANmirror(graphics=FALSE, ind=66)
@@ -98,7 +101,7 @@ numeric_only <- c("Total Cholesterol (mg/dL","Triglyceride (mg/dL",
 id_var <- "Respondent sequence number"
 
 # get file names in output_dir
-dir_files <- list.files(file.path(output_dir, "tables"), pattern = "_scores.csv")
+dir_files <- list.files(file.path(output_dir, "tables"), pattern = "_data.csv")
   
 print(paste("data files found:", paste(dir_files, collapse = ", ")))
 
@@ -107,6 +110,7 @@ org_name <- vector(mode = "character")
 org_method <- vector(mode = "character")
 score <- vector(mode = "numeric")
 day <- vector(mode = "character")
+includes_food <- vector(mode = "logical")
 
 
 ##### Iterate through files and populate variables #####
@@ -135,6 +139,11 @@ for (i in 1:length(dir_files)){
         org_method <- c(org_method, rep(my_method,10))
       }
     }
+    if (grepl("food", dat_f, ignore.case = TRUE)){
+      includes_food[i] <- TRUE
+    }else{
+      includes_food[i] <- FALSE
+    }
     
   }else{
     print(paste(dat_f, "is empty"))
@@ -142,7 +151,7 @@ for (i in 1:length(dir_files)){
 
 }
 
-big_table <- data.frame(org_name, org_method, response_var, day, score)
+big_table <- data.frame(org_name, org_method, response_var, day, includes_food, score)
 
 ##### Order response vars such that numeric are 1st and Boolean are 2nd #####
 my_order <- c(
@@ -155,7 +164,7 @@ big_table$response_var <- factor(big_table$response_var, levels = my_order)
 print(unique(big_table$response_var))
 
 #### Analysis ####
-pdf(file.path(output_dir, "graphics", "all_diet_orgs_days.pdf"), width = 18, height = 10)
+# pdf(file.path(output_dir, "graphics", "all_diet_orgs_days.pdf"), width = 18, height = 10)
 ##### ANOVA for days with plot #####
 myLm = lm(score ~ day, data = big_table)
 pval = anova(myLm)$"Pr(>F)"[1]
@@ -165,6 +174,19 @@ g <- ggplot2::ggplot(big_table, aes(x=day, y=score)) +
   ggplot2::geom_boxplot() +
   ggplot2::ylab("Score") +
   ggplot2::ggtitle(paste0("RF Scores: 1 day VS 2 days, ANOVA pval: ",
+                          round(pval,4))) +
+  geom_text(data = means, aes(label = score, y = score + 0.08))
+g
+
+##### ANOVA for includes_food with plot #####
+myLm = lm(score ~ includes_food, data = big_table)
+pval = anova(myLm)$"Pr(>F)"[1]
+means <- aggregate(score ~ includes_food, data = big_table, mean)
+
+g <- ggplot2::ggplot(big_table, aes(x=includes_food, y=score)) + 
+  ggplot2::geom_boxplot() +
+  ggplot2::ylab("Score") +
+  ggplot2::ggtitle(paste0("RF Scores: includes_food VS no_food, ANOVA pval: ",
                           round(pval,4))) +
   geom_text(data = means, aes(label = score, y = score + 0.08))
 g
@@ -256,6 +278,6 @@ num_table <- big_table[big_table$response_var %in% numeric_only,]
 org_meth_plot(num_table, "Nutrition data organization strategies NO TRIG NUM ONLY")
 resp_vars_plot(num_table, "Nutrition data organization strategies NO TRIG NUM ONLY")
 
-dev.off()
+# dev.off()
 
 print("End of R script!")
